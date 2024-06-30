f96a8d5e7933a2d49deef7a9c1e8705467c5cd6c
MAHESHWARAN <maheshwaran99m@gmail.com>
2024-06-30 20:09:24 +0530

    Initial Commit

---
+++ WeatherCity/MainResources/WeatherCityApp.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherCityApp.swift
+//  WeatherCity
+//
+//  Created by MAHESHWARAN on 30/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCityApp: App {
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ WeatherCity/Source/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherCity
+//
+//  Created by MAHESHWARAN on 30/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+  var body: some View {
+    Color.clear
+  }
+}
+
+// MARK: - Preview
+
+#Preview {
+  ContentView()
+}
