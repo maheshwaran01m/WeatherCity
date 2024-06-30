e507545e46aa365e72c57055fbc198c7b3b8759c
MAHESHWARAN <maheshwaran@eam360.com>
2024-06-30 21:11:36 +0530

    Source  - Created Weather Manager

---
+++ WeatherCity/Source/WeatherManager/WeatherManager.swift
@@ -0,0 +1,62 @@
+//
+//  WeatherManager.swift
+//  WeatherCity
+//
+//  Created by MAHESHWARAN on 30/06/24.
+//
+
+import Foundation
+import CoreLocation
+import WeatherKit
+
+class WeatherManager {
+  
+  static let shared = WeatherManager()
+  
+  private let service = WeatherService.shared
+  
+  private(set) var temperatureFormatter: MeasurementFormatter = {
+    $0.numberFormatter.maximumFractionDigits = 0
+    return $0
+  }(MeasurementFormatter())
+}
+
+// MARK: - Current Weather
+
+extension WeatherManager {
+  
+  func currentWeather(for item: WeatherItem) async -> CurrentWeather? {
+    await Task.detached(priority: .userInitiated) {
+      
+      return try? await self.service.weather(for: item.coordinates, including: .current)
+      
+    }.value
+  }
+}
+
+// MARK: - WeatherAttribution
+
+extension WeatherManager {
+  
+  func weatherAttribution() async -> WeatherAttribution? {
+    await Task.detached(priority: .userInitiated) {
+      
+      return try? await self.service.attribution
+      
+    }.value
+  }
+}
+
+// MARK: - WeatherItem
+
+struct WeatherItem {
+  
+  let latitude: Double
+  let longitude: Double
+  
+  var coordinates: CLLocation {
+    .init(latitude: latitude, longitude: longitude)
+  }
+  
+  static let preview = Self.init(latitude: 37.3346, longitude: -122.0090)
+}
