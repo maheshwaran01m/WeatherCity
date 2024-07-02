c1c2b4657f49032521080185c3b1d0bc218bc7fa
MAHESHWARAN <maheshwaran@eam360.com>
2024-07-02 13:57:02 +0530

    Source  - Created Location Manager

--- WeatherCity/Source/WeatherManager/WeatherView.swift
+++ WeatherCity/Source/WeatherManager/WeatherView.swift
@@ -17,17 +17,23 @@ struct WeatherView: View {
   
   @State private var currentWeather: CurrentWeather?
   @State private var isLoading = false
+  
+  @StateObject private var locationManager = LocationManager()
+  @State private var selectedCity: City?
 
   // MARK: - View
   
   var body: some View {
     mainView
-      .task { await fetchWeather() }
+      .task(id: locationManager.currentLocation) { await fetchCity() }
+      .task(id: selectedCity) { await fetchWeather() }
   }
   
   @ViewBuilder
   private var mainView: some View {
-    if let currentWeather, !isLoading {
+    if !locationManager.isAuthorized {
+      locationDeniedView
+    } else if let currentWeather, !isLoading {
       weatherView(for: currentWeather)
     } else {
       placeholderView
@@ -57,6 +63,14 @@ extension WeatherView {
         .font(.title3)
       
       WeatherAttributionView()
+    }
+  }
+  
+  @ViewBuilder
+  private var cityView: some View {
+    if let selectedCity {
+      Text(selectedCity.name)
+        .font(.title)
     }
   }
   
@@ -91,14 +105,49 @@ extension WeatherView {
 
 extension WeatherView {
   
+  private func fetchCity() async {
+    guard let location = locationManager.currentLocation, selectedCity == nil else {
+      return
+    }
+    selectedCity = location
+  }
+  
   private func fetchWeather() async {
+    guard let selectedCity else { return }
     isLoading = true
     
     Task.detached { @MainActor in
       
-      currentWeather = await weatherManager.currentWeather(for: .preview)
+      currentWeather = await weatherManager.currentWeather(for: selectedCity.weatherItem)
     }
     isLoading = false
+  }
+}
+
+// MARK: - Location Denied View
+
+extension WeatherView {
+  
+  private var locationDeniedView: some View {
+    VStack {
+      Image(systemName: "gear")
+        .resizable()
+        .font(.title)
+      
+      Label("Location Services", systemImage: "gear")
+      
+      Text("""
+      1. Tap on the button below to go to `Privacy and Security`
+      2. Tap on `Location Services`
+      3. Locate the `WeatherCity` app and tap on it.
+      4. Change the settings to `While using the App`
+      """)
+      .multilineTextAlignment(.leading)
+      
+      Link("Open Settings", destination: .init(string: UIApplication.openSettingsURLString)!)
+        .buttonStyle(.borderedProminent)
+        .clipShape(.capsule)
+    }
   }
 }
 
